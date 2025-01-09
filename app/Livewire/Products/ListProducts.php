08b84e27b12555d2d01d141fe511819fa4f64ee9
Samuel Panicucci <samuel.panicucci@gmail.com>
2025-01-09 10:18:26 +0100

    Convert query to scope

--- app/Livewire/Products/ListProducts.php
+++ app/Livewire/Products/ListProducts.php
@@ -3,8 +3,6 @@
 namespace App\Livewire\Products;
 
 use App\Models\Product;
-use DB;
-use Illuminate\Contracts\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class ListProducts extends Component
@@ -16,38 +14,7 @@ class ListProducts extends Component
         $longitude = 10.6282204;
 
         $products = Product::select('*')
-            ->joinSub(
-                DB::query()
-                    ->fromSub(
-                        DB::table("product_shop")
-                            ->select([
-                                'product_shop.product_id AS product_id',
-                                'shops.id AS shop_id',
-                                'shops.name AS shop_name',
-                                'shops.address AS shop_address',
-                                'shops.city AS shop_city',
-                                'shops.phone AS shop_phone',
-                                'shops.latitude AS shop_latitude',
-                                'shops.longitude AS shop_longitude',
-                            ])
-                            ->selectRaw('
-                                ROW_NUMBER() OVER(
-                                    PARTITION BY `product_shop`.`product_id`
-                                    ORDER BY ST_Distance_Sphere(
-                                        Point(`shops`.`latitude`, `shops`.`longitude`),
-                                        Point(?, ?)
-                                    )
-                                ) AS `counter`
-                            ', [$latitude, $longitude])
-                            ->join("shops","shops.id","=","product_shop.shop_id"), 
-                        't'
-                    )
-                    ->where('t.counter', '=', 1),
-                'product_shop_nearest',
-                'products.id',
-                '=',
-                'product_shop_nearest.product_id'
-            )
+            ->withNearestShopTo($latitude, $longitude)
             ->paginate(4);
 
         return view('livewire.products.list-products', compact('products'));
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+use DB;
 
 class Product extends Model
 {
@@ -15,5 +18,40 @@ class Product extends Model
     public function shops(): BelongsToMany {
         return $this->belongsToMany(Shop::class)
             ->withPivot('qty');
+    }
+
+    public function scopeWithNearestShopTo(Builder $query, float $latitude, float $longitude): void {
+        $query->joinSub(
+            DB::query()
+                ->fromSub(
+                    DB::table("product_shop")
+                        ->select([
+                            'product_shop.product_id AS product_id',
+                            'shops.id AS shop_id',
+                            'shops.name AS shop_name',
+                            'shops.address AS shop_address',
+                            'shops.city AS shop_city',
+                            'shops.phone AS shop_phone',
+                            'shops.latitude AS shop_latitude',
+                            'shops.longitude AS shop_longitude',
+                        ])
+                        ->selectRaw('
+                            ROW_NUMBER() OVER(
+                                PARTITION BY `product_shop`.`product_id`
+                                ORDER BY ST_Distance_Sphere(
+                                    Point(`shops`.`latitude`, `shops`.`longitude`),
+                                    Point(?, ?)
+                                )
+                            ) AS `counter`
+                        ', [$latitude, $longitude])
+                        ->join("shops","shops.id","=","product_shop.shop_id"), 
+                    't'
+                )
+                ->where('t.counter', '=', 1),
+            'product_shop_nearest',
+            'products.id',
+            '=',
+            'product_shop_nearest.product_id'
+        );
     }
 }
